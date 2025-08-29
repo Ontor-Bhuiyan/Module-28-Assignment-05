72ee638dd2b719198ee992b9689802aa0bb21477
NurMahammad Ontor <nurmahammadontor@gmail.com>
2025-08-29 13:48:31 +0600

    copy count section work done with mobile responsive

--- js/custom.js
+++ js/custom.js
@@ -12,3 +12,26 @@ document.querySelectorAll('.heart-count-button').forEach(function(button){
     });
 });
 // 1.heart counter section end
+
+// 2.copy count and number paste section start
+let copyCountVar = 0;
+document.getElementById('copy-count').innerText = copyCountVar;
+
+document.querySelectorAll('.copy-button').forEach(function(button){
+    
+    button.addEventListener('click', function(){
+        button.disabled = true;
+
+        const serviceName = button.getAttribute('data-service-name');
+        const serviceNumber = button.getAttribute('data-service-number');
+
+        alert(`⿻ ${serviceName} - ${serviceNumber}...`);
+
+        copyCountVar = copyCountVar + 1;
+        document.getElementById('copy-count').innerText = copyCountVar;
+
+        const copyNumber = document.getElementById('copy-number').innerText;
+        navigator.clipboard.writeText(copyNumber);
+    });
+});
+// 2.copy count and number paste section end
